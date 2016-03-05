902e86cae5f5b46199115a5dcf5a30211aa15b30
歐陽 <ouyang@ouyangdemacbook-air.local>
2016-03-05 23:01:59 +0800

    Init

---
+++ TestGitHub/TestGitHub/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestGitHub
+//
+//  Created by 歐陽 on 16/3/5.
+//  Copyright © 2016年 歐陽. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
